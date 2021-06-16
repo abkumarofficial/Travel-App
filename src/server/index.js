4ce5a877a4c911e186ae0b5c6ef304561091834e
Abhijeet Kumar <abkumarofficial@gmail.com>
2021-06-16 13:51:14 -0700

    Hardcoded environment variables

--- src/server/index.js
+++ src/server/index.js
@@ -9,9 +9,13 @@ const dotenv = require('dotenv');
 dotenv.config({ path: "/home/abhikumar/gitWorkSpace/Travel-App/.env" });
 
 // const applicationKey = process.env.API_KEY
-const geonameUsername = process.env.GEONAME_USERNAME;
-const weatherBitAPIKey = process.env.WEATHERBIT_API_KEY;
-const pixabayApiKey = process.env.PIXABAY_API_KEY;
+// const geonameUsername = process.env.GEONAME_USERNAME;
+// const weatherBitAPIKey = process.env.WEATHERBIT_API_KEY;
+// const pixabayApiKey = process.env.PIXABAY_API_KEY;
+
+const geonameUsername = "abhikumar";
+const weatherBitAPIKey = "73d17426066149009e137e24bd126743";
+const pixabayApiKey = "22070441-4f5dca4dd5453f8f08d7bf9f2";
 const app = express()
 
 const obj = {
